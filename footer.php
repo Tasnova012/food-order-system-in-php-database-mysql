680e76eef83a7332cc3932092c0659d1e6a3aaca
Tasnova Tasnim prity <tasnimprity12@gmail.com>
2021-09-22 04:51:57 -0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,51 @@
+<div class="container-fluid animatedParent" style="background:#F8F9FA; padding:40px;"> 
+		    <div class="container">
+		      
+			      <div class="row animated fadeIn">
+				      
+					  
+					  <div class="col-sm-6" style=" border-right:1px solid black;">
+					    <ul>
+                          <li><h5>ADMIN SECTION</h5></li>
+						  <li><a href="admin.php">Admin Login</a></li>
+						  
+						</ul>
+					  </div>
+					  <!-- social Section Starts Here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
+                </li>
+				<li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/github.png" /></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+					  <div class="col-sm-6" style=" border-right:1px solid black;">
+					     <ul>
+						  <li><h5>HOTEL LINKS</h5></li>
+						  <li><a href="vendor-new.php">Register On Food Hunt</a></li>
+						  <li><a href="vendor_login.php">Hotel Account Login</a></li>
+						  <li><a href="food.php">Add Foods</a></li>
+						  
+						</ul>
+					  </div>
+				  </div>
+			 </div>
+			 	<section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">tasnova tasnim</a></p>
+        </div>
+    </section>		 
+		  </div>
